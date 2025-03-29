905d2884ed797fb3f99556842a14f4fd3761f8f2
somuraj07 <somasankar33@gmail.com>
2025-03-29 09:08:28 +0530

    auth complete

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+
+const SetupPage =() => {
+    return (
+  <div className="p-8">
+   <UserButton />
+  </div>
+  
+    );
+  }
+  export default SetupPage;
